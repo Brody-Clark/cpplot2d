93dd2d3e32764600a301dc766c579fb97a818cac
Brody Clark <brodyclark97@gmail.com>
2025-02-01 20:36:56 -0500

    finished logic and updated icon

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-# <img src="./icon.png" alt="Project Icon" width="64" height="64" align="center"/> pltcpp
+# !["Logo"](C2PPlot.png)
 
-This is a lightweight, header-only 2-D C++ plotting library meant to provide minimalistic line plotting for quick visualization of data. It is designed to work without needing any third party libraries or tools installed. It also includes a print and export feature to print plots to pdf and export to CSV files respectively.
+This is a lightweight, easy to use, header-only 2-D C++ plotting library meant to provide basic line plotting for quick visualization of data. It is designed to work without needing any third party libraries or tools installed. It also includes a print and export feature to save plots as .png files and export to CSV files respectively.
 
 ## How to use
 
-To use in your project, simply add the header file found in the `src` folder to your own project folder and add `#include "pltcpp.hpp"` to the source files that require it. The following code snippet demonstrates how to plot:
+To use in your project, simply add the header file found in the `c2pplot/src` folder to your own project folder and add `#include "c2pplot.h"` to the source files that require it. The following code snippet demonstrates how to call the plot method:
 
 ```cpp
 
-#include "pltcpp.hpp"
+#include "c2pplot.h"
 
-// Create dataset using pltPoint structures
-std::vector<pltcpp::pltPoint> points;
+// Create dataset as 2-D vector of any numeric type
+std::vector<std::vector<float>> points;
 
-// Call plot function with default labels ('x', 'y')
-pltcpp::plt::Plot(points);
+// Call plot function with default labels ('x', 'y') and default tile ('Plot')
+c2pplot::plt::Plot(points);
 
-// Call plot function with custom labels
-pltcpp::plt::Plot(points, "time", "distance");
+// Call plot function with custom axis labels and title
+c2pplot::plt::Plot(points, "time", "distance", "Distance vs Time");
 
 ```
--- pltcpp/src/Main.cpp
+++ pltcpp/src/Main.cpp
@@ -20,115 +20,78 @@
 #define PLTCOLOR_BLACK RGB(0,0,0)
 
 #define PLTRECT_COORDS {0, 0, 200, 30}
-#define PLTRECT_BORDEROFFSET double(0.075)
+#define PLTBORDER_OFFSET double(0.105)
 
-std::vector<POINT> testPoints = { {50, 50}, {100, 75}, {150, 150}, {200,100}, {800,90} };
+std::vector<POINT> testPoints = { {-20, -20}, { 50, 50 }, {100, 75}, {150, 150}, {200,100}, {800,90} };
 
-//TODO: REMOVE
-LPCWSTR ConvertToLPCWSTR(const char* charArray)
-{
-	int bufferSize = MultiByteToWideChar(CP_ACP, 0, charArray, -1, NULL, 0);
-	wchar_t* wideString = new wchar_t[bufferSize];
 
-	MultiByteToWideChar(CP_ACP, 0, charArray, -1, wideString, bufferSize);
 
-	return wideString;
-}
+namespace c2pplot {
 
-std::wstring GetTimestamp()
-{
-	auto now = std::chrono::system_clock::now();
-	std::time_t now_time = std::chrono::system_clock::to_time_t(now);
-	std::string timestamp;
-
-	std::tm local_time;
-	localtime_s(&local_time, &now_time);
-	std::ostringstream oss;
-	oss << std::put_time(&local_time, "%Y%m%d%H%M%S");
-	timestamp = oss.str();
-
-	std::wstring wstr(timestamp.begin(), timestamp.end());
-	return wstr;
-}
-
-//TODO: Remove
-std::string wcharToString(const wchar_t* wstr)
-{
-	// Determine the required size for the multibyte character array
-	size_t length = std::wcslen(wstr) * 4 + 1;  // *4 for max bytes per wchar, +1 for null terminator
-	char* str = new char[length];
-
-	std::wcstombs(str, wstr, length);
-	std::string result(str);
-
-	delete[] str;
-
-	return result;
-}
-void InitGDIPlus(Gdiplus::GdiplusStartupInput& gdiplusStartupInput, ULONG_PTR& gdiplusToken)
-{
-	Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
-}
-
-// Function to shutdown GDI+
-void ShutdownGDIPlus(ULONG_PTR gdiplusToken)
-{
-	Gdiplus::GdiplusShutdown(gdiplusToken);
-}
-
-namespace pltcpp {
-
-	double deltaX = 0, deltaY = 0; //TODO: rename (dataspanX dataspanY)
-	static double mouseX = 0.0; //TODO: rename  mouseCoordX
+	double xDataSpan = 0, yDataSpan = 0;
+	double xPlotDataOffset = 0, yPlotDataOffset = 0;
+	static double mouseX = 0.0;
 	static double mouseY = 0.0;
+	int size = 0;
 
-	// TODO: Cleanup
-	void GetLogicalDeltas(const std::vector<POINT>& points, const size_t& size, double& logicalDeltaX, double& logicalDeltaY)
+	struct Point {
+
+	public:
+		Point(double x, double y) : x(x), y(y) {}
+
+		double x;
+		double y;
+	};
+	std::wstring GetTimestamp()
 	{
-		POINT p;
-		int largestX = -(std::numeric_limits<int>::max)(), largestY = -(std::numeric_limits<int>::max)();
-		int smallestX = (std::numeric_limits<int>::max)(), smallestY = (std::numeric_limits<int>::max)();
-		for (int i = 0; i < size; i++)
-		{
-			p = points[i];
-			if (p.x > largestX)
-			{
-				largestX = p.x;
-			}
-			if (p.x < smallestX)
-			{
-				smallestX = p.x;
-			}
-			if (p.y > largestY)
-			{
-				largestY = p.y;
-			}
-			if (p.y < smallestY)
-			{
-				smallestY = p.y;
-			}
-		}
+		auto now = std::chrono::system_clock::now();
+		std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+		std::string timestamp;
 
-		logicalDeltaX = largestX;
-		logicalDeltaY = largestY;
+		std::tm local_time;
+		localtime_s(&local_time, &now_time);
+		std::ostringstream oss;
+		oss << std::put_time(&local_time, "%Y%m%d%H%M%S");
+		timestamp = oss.str();
+
+		std::wstring wstr(timestamp.begin(), timestamp.end());
+		return wstr;
 	}
-	void DrawTextAtPosition(HDC hdc, int x, int y, const std::string& text)
+
+	void InitGDIPlus(Gdiplus::GdiplusStartupInput& gdiplusStartupInput, ULONG_PTR& gdiplusToken)
+	{
+		Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+	}
+
+	void ShutdownGDIPlus(ULONG_PTR gdiplusToken)
+	{
+		Gdiplus::GdiplusShutdown(gdiplusToken);
+	}
+
+	void DrawTextAtPosition(HDC hdc, int x, int y, const std::wstring& text)
 	{
 		SetTextColor(hdc, PLTCOLOR_WHITE);
 		SetBkMode(hdc, TRANSPARENT);
-		TextOut(hdc, x, y, ConvertToLPCWSTR(text.c_str()), static_cast<int>(text.length()));
+		TextOut(hdc, x, y, text.c_str(), static_cast<int>(text.length()));
+	}
+	void DrawVerticalTextAtPosition(HDC hdc, int x, int y, const std::wstring& text)
+	{
+		SetTextColor(hdc, PLTCOLOR_WHITE);
+		SetBkMode(hdc, TRANSPARENT);
+		for (wchar_t ch : text)
+		{
+			TextOut(hdc, x, y, &ch, 1);
+			y += 12;
+		}
 	}
 	void DrawPoints(HDC hdc, RECT rect, const std::vector <POINT>& points, const size_t& size)
 	{
-		//TODO: only need to calculate this delta once, data doesnt change.
-		GetLogicalDeltas(points, size, deltaX, deltaY);
-
-		const double xPlotOffset = rect.right * 0.075;
-		const double yPlotOffset = rect.bottom * 0.075;
-		const double windowDeltaX = rect.right - 2 * xPlotOffset;
-		const double windowDeltaY = rect.bottom - 2 * yPlotOffset;
-		double scaleX = windowDeltaX / deltaX;
-		double scaleY = windowDeltaY / deltaY;
+		const double xPlotOffset = rect.right * PLTBORDER_OFFSET;
+		const double yPlotOffset = rect.bottom * PLTBORDER_OFFSET;
+		const double xPlotSpan = rect.right - 2 * xPlotOffset;
+		const double yPlotSpan = rect.bottom - 2 * yPlotOffset;
+		double xScale = xPlotSpan / (xDataSpan);
+		double yScale = yPlotSpan / yDataSpan;
 
 		HPEN hPen = CreatePen(PS_SOLID, 1, PLTCOLOR_GREEN);
 		SelectObject(hdc, hPen);
@@ -136,8 +99,8 @@ namespace pltcpp {
 		for (int i = 0; i < size; i++)
 		{
 			p = points[i];
-			int x = static_cast<int>((p.x * scaleX) + xPlotOffset);
-			int y = static_cast<int>(windowDeltaY - (p.y * scaleY) + yPlotOffset);
+			int x = static_cast<int>(((p.x - xPlotDataOffset) * xScale) + xPlotOffset);
+			int y = static_cast<int>(yPlotSpan - ((p.y - yPlotDataOffset) * yScale) + yPlotOffset);
 
 			if (i > 0)
 			{
@@ -150,31 +113,33 @@ namespace pltcpp {
 		}
 		DeleteObject(hPen);
 	}
-
-	RECT GetPlotRect(RECT rect)
+	void DrawVertcialTick(HDC hdc, RECT rect, const int tickCenterX, const int tickCenterY, const double tickValue, const int tickLength)
 	{
-		RECT out;
-		const int yBorderOffset = static_cast<int>(rect.bottom * PLTRECT_BORDEROFFSET);
-		const int xBorderOffset = static_cast<int>(rect.right * PLTRECT_BORDEROFFSET);
+		MoveToEx(hdc, tickCenterX, tickCenterY - tickLength, NULL);
+		LineTo(hdc, tickCenterX, tickCenterY + tickLength);
+		std::wstringstream label;
+		label << std::setprecision(4) << tickValue;
+		DrawTextAtPosition(hdc, tickCenterX - 10, min(tickCenterY + 10, rect.bottom - 20), label.str());
 
-		out.left = rect.left + xBorderOffset;
-		out.right = rect.right - xBorderOffset;
-		out.top = rect.top + yBorderOffset;
-		out.bottom = rect.bottom - yBorderOffset;
-
-		return out;
 	}
+	void DrawHorizontalTick(HDC hdc, RECT rect, const int tickCenterX, const int tickCenterY, const double tickValue, const int tickLength)
+	{
+		MoveToEx(hdc, tickCenterX - tickLength, tickCenterY, NULL);
+		LineTo(hdc, tickCenterX + tickLength, tickCenterY);
+		std::wstringstream label;
+		label << std::setprecision(4) << tickValue;
+		DrawTextAtPosition(hdc, max(rect.left + 10, tickCenterX - 30), tickCenterY - 5, label.str());
 
-	void DrawAxes(HDC hdc, RECT rect)
+	}
+	void DrawAxes(HDC hdc, RECT rect, const Point& plotOffset)
 	{
 		HPEN hPen = CreatePen(PS_SOLID, 1, PLTCOLOR_WHITE);
 		SelectObject(hdc, hPen);
 
-		RECT borderRect = GetPlotRect(rect);
-		const int leftBorderPos = borderRect.left;
-		const int rightBorderPos = borderRect.right;
-		const int topBorderPos = borderRect.top;
-		const int bottomBorderPos = borderRect.bottom;
+		const int leftBorderPos = static_cast<int>(rect.left + plotOffset.x);
+		const int rightBorderPos = static_cast<int>(rect.right - plotOffset.x);
+		const int topBorderPos = static_cast<int>(rect.top + plotOffset.y);
+		const int bottomBorderPos = static_cast<int>(rect.bottom - plotOffset.y);
 
 		// Draw the border
 		MoveToEx(hdc, leftBorderPos, topBorderPos, NULL);
@@ -183,48 +148,48 @@ namespace pltcpp {
 		LineTo(hdc, leftBorderPos, bottomBorderPos);
 		LineTo(hdc, leftBorderPos, topBorderPos);
 
-		int tickLength = 5;
+		// Draw labels
+		DrawTextAtPosition(hdc, rect.right / 2, rect.bottom - 20, L"XXTTJJ");
+		DrawVerticalTextAtPosition(hdc, rect.left + 10, rect.bottom / 2, L"XXTTJJ");
 
+		int tickLength = 4;
+		std::wstringstream label;
 		// Draw X-axis ticks
 		int numTicksX = 4;
 		int x = 0;
+		double offset = xPlotDataOffset;
+		double increment = xDataSpan / (numTicksX + 1);
 		for (int i = 1; i <= numTicksX; ++i)
 		{
 			x = leftBorderPos + i * (rightBorderPos - leftBorderPos) / (numTicksX + 1);
-			MoveToEx(hdc, x, bottomBorderPos + 5, NULL);
-			LineTo(hdc, x, bottomBorderPos - tickLength);
-
-			// Draw labels beneath tick marks
-			std::ostringstream label;
-			label << i;
-			DrawTextAtPosition(hdc, x - 3, min(bottomBorderPos + 10, rect.bottom - 20), label.str());
+			offset += increment;
+			DrawVertcialTick(hdc, rect, x, bottomBorderPos, offset, tickLength);
 		}
+		DrawVertcialTick(hdc, rect, rightBorderPos, bottomBorderPos, offset + increment, tickLength);
 
 		// Draw Y-axis ticks
 		int numTicksY = 4;
 		int y = 0;
+		increment = yDataSpan / (numTicksY + 1);
+		offset = yPlotDataOffset;
 		for (int i = 1; i <= numTicksY; ++i)
 		{
 			y = topBorderPos + i * (bottomBorderPos - topBorderPos) / (numTicksY + 1);
-			MoveToEx(hdc, leftBorderPos - 5, y, NULL);
-			LineTo(hdc, leftBorderPos + tickLength, y);
-
-			// Draw labels next to tick marks
-			std::ostringstream label;
-			label << i;
-			DrawTextAtPosition(hdc, max(rect.left + 10, leftBorderPos - 20), y - 5, label.str());
+			offset += increment;
+			DrawHorizontalTick(hdc, rect, leftBorderPos, y, offset, tickLength);
 		}
+		DrawHorizontalTick(hdc, rect, leftBorderPos, topBorderPos, offset + increment, tickLength);
 
 		DeleteObject(hPen);
 	}
-	void DrawPlot(HDC hdc, RECT rect, const std::vector<POINT>& points)
+	void DrawPlot(HDC hdc, RECT rect, Point plotOffsets)
 	{
 		HBRUSH blackBrush = CreateSolidBrush(PLTCOLOR_BLACK);
 		FillRect(hdc, &rect, blackBrush);
 		DeleteObject(blackBrush);
 
-		DrawAxes(hdc, rect);
-		DrawPoints(hdc, rect, points, points.size());
+		DrawAxes(hdc, rect, plotOffsets);
+		DrawPoints(hdc, rect, testPoints, testPoints.size());
 	}
 
 	void DrawCoordinates(HDC hdc, RECT rect, double x, double y)
@@ -265,34 +230,58 @@ namespace pltcpp {
 
 		return hbmScreen;
 	}
-
-	bool IsPointInRect(const int& x, const int& y, RECT rect)
+	void Plot(const std::vector<POINT>& points, const std::string& title = "Plot", const std::string& xLabel = "x", const std::string& yLabel = "y")
 	{
-		return x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom;
-	}
+		int size = points.size();
 
+		POINT p;
+		int largestX = -(std::numeric_limits<int>::max)(), largestY = -(std::numeric_limits<int>::max)();
+		int smallestX = (std::numeric_limits<int>::max)(), smallestY = (std::numeric_limits<int>::max)();
+		for (int i = 0; i < size; i++)
+		{
+			p = points[i];
+			if (p.x > largestX)
+			{
+				largestX = p.x;
+			}
+			if (p.x < smallestX)
+			{
+				smallestX = p.x;
+			}
+			if (p.y > largestY)
+			{
+				largestY = p.y;
+			}
+			if (p.y < smallestY)
+			{
+				smallestY = p.y;
+			}
+		}
+
+		xDataSpan = largestX - smallestX;
+		yDataSpan = largestY - smallestY;
+
+		// The conversion from data-space to plot-space requires both coord systems to start from (0, 0).
+		// These values will be used to adjust the data points during conversion to achieve this.
+		xPlotDataOffset = smallestX;
+		yPlotDataOffset = smallestY;
+
+	}
 	LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
+		RECT rect;
+		GetClientRect(hwnd, &rect);
+		const double xPlotOffset = rect.right * PLTBORDER_OFFSET;
+		const double yPlotOffset = rect.bottom * PLTBORDER_OFFSET;
+		Point plotOffsets(rect.right * PLTBORDER_OFFSET, rect.bottom * PLTBORDER_OFFSET);
 		switch (uMsg)
 		{
 		case WM_PAINT: {
 			PAINTSTRUCT ps;
 			HDC hdc = BeginPaint(hwnd, &ps);
-			RECT rect;
-			GetClientRect(hwnd, &rect);
-			/*		HDC hdcMem = CreateCompatibleDC(hdc);
-					HBITMAP hbmMem = CreateCompatibleBitmap(hdc, rect.right, rect.bottom);
-					HGDIOBJ hOld = SelectObject(hdcMem, hbmMem);*/
-			DrawPlot(hdc, rect, testPoints);
+
+			DrawPlot(hdc, rect, plotOffsets);
 			DrawCoordinates(hdc, rect, mouseX, mouseY);
-			//DrawToMemoryDC(hwnd, hdc);
-			//BitBlt(hdc, 0, 0, rect.right, rect.bottom, hdcMem, 0, 0, SRCCOPY);
-
-			////// Clean up
-			//SelectObject(hdcMem, hOld);
-			//DeleteObject(hbmMem);
-			//DeleteDC(hdcMem);
-
 			EndPaint(hwnd, &ps);
 		}
 					 return 0;
@@ -317,33 +306,32 @@ namespace pltcpp {
 			}
 			break;
 		}
-					   /*case WM_GETMINMAXINFO: {
-						   MINMAXINFO* minMaxInfo = (MINMAXINFO*)lParam;
-						   minMaxInfo->ptMinTrackSize.x = 400;
-						   minMaxInfo->ptMinTrackSize.y = 300;
-					   }
-											break;*/
+		case WM_GETMINMAXINFO: {
+			MINMAXINFO* minMaxInfo = (MINMAXINFO*)lParam;
+			minMaxInfo->ptMinTrackSize.x = 200;
+			minMaxInfo->ptMinTrackSize.y = 100;
+		}
+							 break;
 		case WM_MOUSEMOVE: {
 			// Get the mouse position
 			int x = LOWORD(lParam);
 			int y = HIWORD(lParam);
 			RECT textRect = PLTRECT_COORDS;
-			RECT rect;
-			GetClientRect(hwnd, &rect);
-			RECT plotRect = GetPlotRect(rect);
-			if (IsPointInRect(x, y, plotRect))
+
+			const double xPlotOffset = rect.right * PLTBORDER_OFFSET;
+			const double yPlotOffset = rect.bottom * PLTBORDER_OFFSET;
+			if ((x >= xPlotOffset && x <= rect.right - xPlotOffset) &&
+				(y >= yPlotOffset && y <= rect.bottom - yPlotOffset))
 			{
-				const double xPlotOffset = rect.right * 0.075;
-				const double yPlotOffset = rect.bottom * 0.075;
-				const double windowDeltaX = rect.right - 2 * xPlotOffset;
-				const double windowDeltaY = rect.bottom - 2 * yPlotOffset;
-				double scaleX = deltaX / windowDeltaX;
-				double scaleY = deltaY / windowDeltaY;
+				const double xPlotSpan = rect.right - 2 * xPlotOffset;
+				const double yPlotSpan = rect.bottom - 2 * yPlotOffset;
+				double xScale = xDataSpan / xPlotSpan;
+				double yScale = yDataSpan / yPlotSpan;
 				// Convert device coordinates to logical coordinates
 
 
-				mouseX = (x - xPlotOffset) * scaleX;
-				mouseY = (windowDeltaY - y + yPlotOffset) * scaleY;
+				mouseX = (x - xPlotOffset) * xScale + xPlotDataOffset;
+				mouseY = (yPlotSpan - y + yPlotOffset) * yScale + yPlotDataOffset;
 				// Invalidate the window to trigger a repaint
 				InvalidateRect(hwnd, &textRect, TRUE);
 				break;
@@ -376,61 +364,87 @@ namespace pltcpp {
 		return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 
-} // tnypltcpp
+	int Init(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+	{
+		Plot(testPoints);
+
+		Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+		ULONG_PTR gdiplusToken;
+		InitGDIPlus(gdiplusStartupInput, gdiplusToken);
+
+		const wchar_t CLASS_NAME[] = L"PlotWindowClass";
+
+		WNDCLASS wc = {};
+		wc.lpfnWndProc = c2pplot::WindowProc;
+		wc.hInstance = hInstance;
+		wc.lpszClassName = CLASS_NAME;
+		wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+
+		RegisterClass(&wc);
+
+		HWND hwnd = CreateWindowEx(
+			0,
+			CLASS_NAME,
+			L"Simple 2D Plot",
+			WS_OVERLAPPEDWINDOW,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			800,
+			600,
+			NULL,
+			NULL,
+			hInstance,
+			NULL);
+
+		if (hwnd == NULL)
+		{
+			return 0;
+		}
+
+		// TODO: Header only file wouldnt have acccess to resources folder anyways
+		/*HICON hIcon = (HICON)LoadImage(
+			NULL,
+			L".\\resources\\c2pplot.png",
+			IMAGE_ICON,
+			0,
+			0,
+			LR_LOADFROMFILE | LR_DEFAULTSIZE
+		);
+
+		if (hIcon == NULL)
+		{
+			return 1;
+		}
+		SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+		SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);*/
+
+		// Create the menu
+		HMENU hMenu = CreateMenu();
+		HMENU hFileMenu = CreateMenu();
+
+		AppendMenu(hFileMenu, MF_STRING, PLTMENU_SAVE, L"Save");
+		AppendMenu(hFileMenu, MF_STRING, PLTMENU_EXPORT, L"Export To CSV");
+		AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, L"File");
+
+		SetMenu(hwnd, hMenu);
+
+		ShowWindow(hwnd, nCmdShow);
+		UpdateWindow(hwnd);
+
+		MSG msg = {};
+		while (GetMessage(&msg, NULL, 0, 0))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		ShutdownGDIPlus(gdiplusToken);
+		//DestroyIcon(hIcon);
+		return 0;
+	}
+
+} // c2pplot
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
-	ULONG_PTR gdiplusToken;
-	InitGDIPlus(gdiplusStartupInput, gdiplusToken);
-
-	const wchar_t CLASS_NAME[] = L"PlotWindowClass";
-
-	WNDCLASS wc = {};
-	wc.lpfnWndProc = pltcpp::WindowProc;
-	wc.hInstance = hInstance;
-	wc.lpszClassName = CLASS_NAME;
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-
-	RegisterClass(&wc);
-
-	HWND hwnd = CreateWindowEx(
-		0,
-		CLASS_NAME,
-		L"Simple 2D Plot",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		800,
-		600,
-		NULL,
-		NULL,
-		hInstance,
-		NULL);
-
-	if (hwnd == NULL)
-	{
-		return 0;
-	}
-	// Create the menu
-	HMENU hMenu = CreateMenu();
-	HMENU hFileMenu = CreateMenu();
-
-	AppendMenu(hFileMenu, MF_STRING, PLTMENU_SAVE, L"Save");
-	AppendMenu(hFileMenu, MF_STRING, PLTMENU_EXPORT, L"Export To CSV");
-	AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, L"File");
-
-	SetMenu(hwnd, hMenu);
-
-	ShowWindow(hwnd, nCmdShow);
-	UpdateWindow(hwnd);
-
-	MSG msg = {};
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-	ShutdownGDIPlus(gdiplusToken);
-	return 0;
+	return c2pplot::Init(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
 }
