e119e5ee2ae1d66b3551a6015a762d9df17bd6f1
Brody Clark <78869517+Brody-Clark@users.noreply.github.com>
2025-12-28 21:11:19 -0500

    Added X11 Window Implementation (#5)
    * Added X11 implementation. Removed PNG-specific image requirement. Updated README. Added docstring at top of lib
    
    * update unit test exe in cmakelists

--- tests/unit/test_cpplot2d.cpp
+++ tests/unit/test_cpplot2d.cpp
@@ -1,7 +1,29 @@
 #include <catch2/catch_test_macros.hpp>
 #include <vector>
 #define CPPLOT2D_IMPLEMENTATION
+#define CPPLOT2D_HEADLESS
 #include "../../include/cpplot2d.h"
+
+class TestPlot2D : public cpplot2d::Plot2D
+{
+    public:
+    TestPlot2D(std::string title = "", std::string xLabel = "", std::string yLabel = "",
+           cpplot2d::PlotProperties props = {}) : Plot2D(title, xLabel, yLabel, props)
+    {
+
+    }
+
+    IWindow* GetMockWindow() {return m_window.get();}
+    protected:
+
+    class MockWindow : public cpplot2d::Plot2D::IWindow
+    {
+        public:
+        MockWindow();
+        ~MockWindow() override{}
+
+    };
+};
 
 TEST_CASE("Basic test", "[plot]")
 {
@@ -12,6 +34,6 @@ TEST_CASE("Basic test", "[plot]")
         x[i] = static_cast<float>(i) * 0.1f;
         y[i] = static_cast<float>(i) * 0.1f;
     }
-    cpplot2d::Plot2D plot;
+    TestPlot2D plot;
     CHECK(true);
 }
